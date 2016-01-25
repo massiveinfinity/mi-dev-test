84ae4fab8874b0977f6b4fcf0f0677b700e9f47e
Ilanthirayan <theebankala@gmail.com>
2016-01-25 23:37:37 +0800

    Device List Activity class has been created

---
+++ app/src/main/java/com/infinity/massive/view/activity/DeviceListActivity.java
@@ -0,0 +1,141 @@
+package com.infinity.massive.view.activity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.infinity.massive.R;
+
+public class DeviceListActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String TAG = DeviceListActivity.class.getSimpleName();
+
+    private static final long DRAWER_CLOSE_DELAY_MS = 250;
+    private static final String NAV_ITEM_ID = "navItemId";
+
+    private Fragment fragment;
+
+    private DrawerLayout mDrawerLayout;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+
+    private ActionBarDrawerToggle mDrawerToggle;
+    private int mNavItemId;
+    private final Handler mDrawerActionHandler = new Handler();
+
+
+    @Override
+    public void onBackPressed(){
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+            return;
+        }
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_list);
+
+        if(android.os.Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT){
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.main_drawer_layout);
+
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        // load saved navigation state if present
+        if (null == savedInstanceState) {
+            mNavItemId = R.id.nav_home;
+        }
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        if (navigationView != null) {
+            navigationView.setNavigationItemSelectedListener(this);
+        }
+
+        // set up the hamburger icon to open and close the drawer
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.open,
+                R.string.close);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+
+        //initialise drawer menu selection
+        navigate(mNavItemId);
+
+        fragment = DeviceListFragment.getInstance(this);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.home_screen_container, fragment).commit();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_device_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+        mNavItemId = menuItem.getItemId();
+
+        // allow some time after closing the drawer before performing real navigation
+        // so the user can see what is happening
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        mDrawerActionHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                navigate(mNavItemId);
+            }
+        }, DRAWER_CLOSE_DELAY_MS);
+        return true;
+    }
+
+
+    private void navigate(final int itemId) {
+        // select the correct nav menu item
+        navigationView.getMenu().findItem(R.id.nav_home).setChecked(true);
+
+        switch(itemId){
+            case R.id.nav_home:
+                break;
+            default:
+                break;
+        }
+    }
+}
