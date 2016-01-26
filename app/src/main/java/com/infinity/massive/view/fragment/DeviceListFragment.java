4f9c632056deae2ef972dd662b62a3dc25e087a7
Ilanthirayan <theebankala@gmail.com>
2016-01-26 12:56:23 +0800

    render the device list after downloading finish is implemented

--- app/src/main/java/com/infinity/massive/view/fragment/DeviceListFragment.java
+++ app/src/main/java/com/infinity/massive/view/fragment/DeviceListFragment.java
@@ -17,14 +17,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.infinity.massive.ApplicationMassiveInfinity;
-import com.infinity.massive.model.Devices;
+import com.infinity.massive.R;
+import com.infinity.massive.controller.service.DeviceDownloadService;
+import com.infinity.massive.model.adapter.DeviceListViewAdapter;
+import com.infinity.massive.model.pojo.Devices;
 import com.infinity.massive.utils.DataBaseClient;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by Ilanthirayan on 25/1/16.
@@ -33,7 +34,7 @@ public class DeviceListFragment extends Fragment{
 
     private static final String TAG = DeviceListFragment.class.getSimpleName();
 
-    private ProductGridViewAdapter adapter;
+    private DeviceListViewAdapter adapter;
     private ProgressBar progressBar;
     private TextView emptyListText;
     private Activity mActivity;
@@ -41,16 +42,7 @@ public class DeviceListFragment extends Fragment{
     private RecyclerView mRecyclerView;
 
     private GridLayoutManager mGridLayoutManager ;
-    private List<Devices> productsList;
-
-    final AtomicInteger loadingPageNumber = new AtomicInteger(0);
-
-    private int previousTotal = 0; // The total number of items in the dataset after the last load
-    private boolean loading = true; // True if we are still waiting for the last set of data to load.
-    private int visibleThreshold = 1; // The minimum amount of items to have below your current scroll position before loading more.
-    int firstVisibleItem, visibleItemCount, totalItemCount;
-
-    private int resultSize = 20;//initial value 20 for the default
+    private List<Devices> devicesList;
 
 
     public static DeviceListFragment getInstance(Activity mActivity){
@@ -75,7 +67,7 @@ public class DeviceListFragment extends Fragment{
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        ProductDownloadService.start(false, null);
+        DeviceDownloadService.start(false, null);
     }
 
     protected final void displayEmptyOrList() {
@@ -84,7 +76,7 @@ public class DeviceListFragment extends Fragment{
         mActivity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                final int downloading = ProductDownloadService.getRemainingBlockRenderProductLayoutDownloads();
+                final int downloading = DeviceDownloadService.getRemainingBlockRenderProductLayoutDownloads();
 
                 if (downloading > 0) {
                     progressBar.setVisibility(View.VISIBLE);
@@ -94,7 +86,7 @@ public class DeviceListFragment extends Fragment{
                     return;
                 }
 
-                if (!DataBaseClient.getInstance().hasAnyProducts()) {
+                if (!DataBaseClient.getInstance().hasAnyDevices()) {
                     progressBar.setVisibility(View.GONE);
                     mRecyclerView.setVisibility(View.GONE);
                     emptyListText.setVisibility(View.VISIBLE);
@@ -115,112 +107,29 @@ public class DeviceListFragment extends Fragment{
 
     private void initializeAdapter(){
         //initialise here otherwise list will be null
-        productsList = DataBaseClient.getInstance().getProducts();
-        adapter = new ProductGridViewAdapter(getActivity(), productsList);
+        devicesList = DataBaseClient.getInstance().getDevices();
+        adapter = new DeviceListViewAdapter(getActivity(), devicesList);
         mRecyclerView.setAdapter(adapter);
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ViewGroup view = (ViewGroup)inflater.inflate(R.layout.fragment_product, container, false);
+        ViewGroup view = (ViewGroup)inflater.inflate(R.layout.fragment_device, container, false);
 
-        progressBar = (ProgressBar) view.findViewById(R.id.progressProductLayout);
-        emptyListText = (TextView) view.findViewById(R.id.product_empty_list_msg);
+        progressBar = (ProgressBar) view.findViewById(R.id.progressDeviceLayout);
+        emptyListText = (TextView) view.findViewById(R.id.device_empty_list_msg);
         ApplicationMassiveInfinity.getLbm().registerReceiver(downloadFinishedReceiver,
-                new IntentFilter(ProductDownloadService.ACTION_BLOCK_RENDER_PRODUCT_DOWNLOAD_FINISHED));
+                new IntentFilter(DeviceDownloadService.ACTION_BLOCK_RENDER_DEVICE_DOWNLOAD_FINISHED));
 
         //CARD VIEW
-        mRecyclerView = (RecyclerView) view.findViewById(R.id.product_recycle_list);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.device_recycle_list);
         mGridLayoutManager = new GridLayoutManager(getActivity(), 2);
         mRecyclerView.setLayoutManager(mGridLayoutManager);
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
 
-        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-
-                visibleItemCount = mRecyclerView.getChildCount();
-                totalItemCount = mGridLayoutManager.getItemCount();
-                firstVisibleItem = mGridLayoutManager.findFirstVisibleItemPosition();
-
-                if (loading) {
-                    if (totalItemCount > previousTotal + 1) {
-                        loading = false;
-                        previousTotal = totalItemCount;
-                    }
-                }
-                if (!loading && (totalItemCount - visibleItemCount) <= (firstVisibleItem + visibleThreshold)) {
-                    // End has been reached
-                    // Do something
-                    loadingPageNumber.incrementAndGet();
-                    onLoadMore();
-                    loading = true;
-                }
-            }
-        });
-
         displayEmptyOrList();
 
         return view;
-    }
-
-    public void onLoadMore() {
-        if (!Utils.isOnline())
-            return;
-
-        if (resultSize < 20) {
-            return;
-        }
-
-        //add null , so the adapter will check view_type and show progress bar at bottom
-        productsList.add(null);
-        mGridLayoutManager.setSpanCount(1);
-        adapter.notifyItemInserted(productsList.size() - 1);
-
-        ProductController mProductController = new ProductController();
-        Call<ProductsParentResponse> response = mProductController.setProductsService(loadingPageNumber.get(), 20);
-        response.enqueue(new Callback<ProductsParentResponse>() {
-            @Override
-            public void onResponse(Response<ProductsParentResponse> response, Retrofit retrofit) {
-                Log.d(TAG, "Response Raw :: " + response.raw());
-                if (response.body().getStatus().getCode() == 0) {
-                    //SUCCESSFULLY PRODUCTS RECEIVE
-
-                    //remove the progress item
-                    productsList.remove(productsList.size() - 1);
-                    mGridLayoutManager.setSpanCount(2);
-                    adapter.notifyItemRemoved(productsList.size());
-
-                    //SAVE THE PRODUCTS IN TO THE DATABASE
-                    DataBaseClient.getInstance().insertProducts(response.body(), false);
-
-                    for (Products products : response.body().getProducts()) {
-                        productsList.add(products);
-                        adapter.notifyItemInserted(productsList.size());
-                    }
-
-                } else {
-                    //ERROR MESSAGE DIALOG
-                    Toast.makeText(ApplicationMassiveInfinity.getContext(),
-                            response.body().getStatus().getMsg(), Toast.LENGTH_LONG).show();
-                }
-
-            }
-
-            @Override
-            public void onFailure(Throwable e) {
-                Log.d(TAG, "onFailure: " + e.getMessage());
-                //remove the progress item
-                productsList.remove(productsList.size() - 1);
-                mGridLayoutManager.setSpanCount(2);
-                adapter.notifyItemRemoved(productsList.size());
-            }
-        });
-
-    }
-
-    public void setScrollPosition(final int mScrollPosition){
-        mGridLayoutManager.scrollToPosition(mScrollPosition);
     }
 }
