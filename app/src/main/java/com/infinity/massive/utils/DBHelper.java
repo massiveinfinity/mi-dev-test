8470211ed17c2b5fc8c440e157c2a9c791116dfa
Ilanthirayan <theebankala@gmail.com>
2016-01-26 12:44:24 +0800

    create device table query is added

--- app/src/main/java/com/infinity/massive/utils/DBHelper.java
+++ app/src/main/java/com/infinity/massive/utils/DBHelper.java
@@ -5,6 +5,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 import com.infinity.massive.ApplicationMassiveInfinity;
+import com.infinity.massive.model.pojo.Devices;
 
 /**
  * Created by Ilanthirayan on 25/1/16.
@@ -29,10 +30,29 @@ public class DBHelper extends SQLiteOpenHelper {
         return INSTANCE.INSTANCE.instance;
     }
 
+    private static final String CREATE_TABLE_DEVICE = "CREATE TABLE "
+            + Devices.TABLE_NAME + " ("
+            + Devices.Columns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+            + Devices.Columns.DEVICE_ID + " INTEGER, "
+            + Devices.Columns.IMAGE_URL + " TEXT, "
+            + Devices.Columns.NAME + " TEXT, "
+            + Devices.Columns.ANDROID_ID + " TEXT, "
+            + Devices.Columns.SNIPPET + " TEXT); ";
+
+    private static final String CREATE_DEVICE_INDEX_ID = CREATE_INDEX
+            + " index_based_id ON "
+            + Devices.TABLE_NAME + "("
+            + Devices.Columns.DEVICE_ID + ");";
+
     @Override
     public void onCreate(SQLiteDatabase db) {
         Log.d(TAG, "db onCreate");
         //CREATE TABLES
+        db.execSQL(CREATE_TABLE_DEVICE);
+
+        //CREATE INDICES
+        db.execSQL(CREATE_DEVICE_INDEX_ID);
+
     }
 
     @Override
